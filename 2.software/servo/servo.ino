e2c333812051c47e7a0a2b85e3688f8c4d79c707
Evence <evencewu@outlook.com>
2022-08-26 22:56:14 +0800

    1

--- 2.software/servo/servo.ino
+++ 2.software/servo/servo.ino
@@ -1,20 +1,34 @@
-#include <Servo.h>            //加载文件库
-int pos = 0;
-Servo myservo;
- 
-void setup()
-{
-  myservo.attach(5, 500, 2500);          //修正脉冲宽度
+#include <Servo.h>
+Servo my1;
+Servo my2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  my1.attach(D1);
+  my2.attach(D2);
+  my1.write(105);
+  my2.write(100);
 }
- 
-void loop()
-{
-  for (pos = 0; pos <= 180; pos += 1) {       //pos+=1等价于pos=pos+1
-    myservo.write(pos);
-    delay(15);          
-  }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-    delay(15);          
-  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0)
+  {
+      char i = Serial.read();
+      int ser = Serial.parseInt();
+      // Serial.parseTint() 会解析出串口内的数字
+      switch(i)
+      {
+        case 'a':
+          my1.write(ser);
+          Serial.println(ser);
+          break;
+        case 'b':
+          my2.write(ser);
+          Serial.println(ser);
+          delay(15);
+          break;
+      }
+   }
 }
